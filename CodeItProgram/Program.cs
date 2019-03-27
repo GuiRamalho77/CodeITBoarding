105b95b521cfbb5263e234f31b3147eb56dd7962
Guilherme Ramalho <guilhermesramalho@gmail.com>
2019-03-27 18:02:14 -0300

    Add files via upload

---
+++ CodeItProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeItProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startup = new Startup();
+            Console.WriteLine("---------- CODEIT AIRLINES  ----------");
+            Console.WriteLine(" Iniciar Aplicacao generica [1] \n Iniciar Aplicacao complexa [2]");
+            int typeOfApplication = Convert.ToInt32(Console.ReadLine());
+
+            var listConsoleWrite = new List<string>();
+            switch (typeOfApplication)
+            {
+                case 1:
+                    listConsoleWrite = startup.simpleProcess();
+                    break;
+                case 2:
+                    listConsoleWrite = startup.complexProcess(startup.genereteInputConsole());
+                    break;
+            }
+
+            foreach (var write in listConsoleWrite)
+                Console.WriteLine(write);
+            Console.WriteLine("DESEJA IMPORTAR O PROCESOS PARA UM ARQUIVO TXT? \n [1] SIM\n [2] NAO");
+            int import = Convert.ToInt32(Console.ReadLine());
+            if (import == 1)
+            {
+                var export = startup.exportFile(listConsoleWrite);
+                if(!string.IsNullOrEmpty(export))
+                Console.WriteLine("Arquivo exportado para: "+export);
+            }
+            Console.WriteLine("PROCESSO FINALIZADO...");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ CodeItProgram/Startup.cs
@@ -0,0 +1,120 @@
+﻿using CodeIt.Business.Business;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CodeItProgram
+{
+    public class Startup
+    {
+        private static Boarding boarding;
+
+        public Startup()
+        {
+            boarding = new Boarding();
+        }
+
+        public List<string> simpleProcess()
+        {
+            return boarding.applyRules();
+        }
+        public List<string> complexProcess(dynamic rangeOfPeople)
+        {
+            return boarding.applyRules(rangeOfPeople);
+        }
+
+        public dynamic genereteInputConsole()
+        {
+            var rangeTripulation = new List<dynamic>();
+            Console.WriteLine("---------- VAMOS COMECAR A INCLUIR A TRIPULACAO ----------");
+
+            Console.WriteLine("Digite o nome do POLICIAL: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 5
+            });
+            Console.WriteLine("Digite o nome do PRESIDIARIO: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 6
+            });
+            Console.WriteLine("Digite o nome do PILOTO: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 1
+            });
+
+            Console.WriteLine("Digite o nome do primeiro OFICIAL: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 3
+            });
+
+            Console.WriteLine("Digite o nome do segundo OFICIAL: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 3
+            });
+
+            Console.WriteLine("Digite o nome CHEFE DE BORDO: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 2
+            });
+
+            Console.WriteLine("Digite o nome da primeira COMISSARIA: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 4
+            });
+
+            Console.WriteLine("Digite o nome da segunda COMISSARIA: ");
+            rangeTripulation.Add(new
+            {
+                Name = Console.ReadLine(),
+                JobType = 4
+            });
+            return rangeTripulation;
+        }
+
+        public string exportFile(List<string> lines)
+        {
+            string folder = @"C:\Temp";
+            if (!Directory.Exists(folder))
+            {
+                Directory.CreateDirectory(folder);
+            }
+            string fileName = $@"{folder}\Boarding.txt";
+            try
+            {
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+                // Create a new file     
+                using (FileStream fs = File.Create(fileName))
+                {
+                    foreach (var line in lines)
+                    {
+                        Byte[] lineWrite = new UTF8Encoding(true).GetBytes(line);
+                        fs.Write(lineWrite);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+                return null;
+            }
+            return fileName;
+        }
+    }
+}
